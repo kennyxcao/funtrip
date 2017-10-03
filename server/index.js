ab1f6bfe516ea352fd77422380bf768034e444d1
Kenny Cao <kennyxcao@gmail.com>
2017-10-02 18:07:27 -0700

    Add morgan package and implement cookie parser function

--- server/index.js
+++ server/index.js
@@ -1,19 +1,34 @@
 'use strict';
 const express = require('express');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const querystring = require('querystring');
-//const db = require('../database/index');
-// const db = require('../database/index');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const Promise = require('bluebird');
+
+//const db = require('../database/index');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cookieParser());
+
+app.get('/', (req, res, next) => {
+  console.log('Hello World');
+  console.log('Request Cookies :', req.cookies);
+  next();
+});
+
+
 app.use(express.static(__dirname + '/../react-client'));
 app.use('/js', express.static(__dirname + '/../node_modules/bootstrap/dist/js'));  // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/../node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/js', express.static(__dirname + '/../node_modules/moment')); // redirect JS Moment
 app.use('/css', express.static(__dirname + '/../node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use('/css', express.static(__dirname + '/../react-client/css')); // redirect custom css file
+
+
 
 app.post('/login', (req, res) => {
   console.log('/login post received');
@@ -22,6 +37,8 @@ app.post('/login', (req, res) => {
 app.get('/user', (req, res) => {
   console.log('/user get received');
 });
+
+
 
 let port = process.env.PORT || 3000;
 
