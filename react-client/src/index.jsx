3ad87b7b2dd5c8834df060313bb39c87f8e387b7
Elena Bartosh <elena.karnaukhova@gmail.com>
2017-10-03 18:46:55 -0700

    Add data from database to preparation list

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -68,7 +68,7 @@ class App extends React.Component {
             loggedIn: true,
             user: results.username
           });  
-        //call next function to get username 
+          //call next function to get username 
           this.updateStateForTrips();        
         } else {
           console.log('Incorrect Login');          
@@ -102,35 +102,35 @@ class App extends React.Component {
     console.log('handleObjAdd', item);
   }
   render () {
+    console.log(this.state);
     return (
-      
-    <div className='.container-fluid'>
-      <nav className='navbar navbar-default bg-faded'>
-       <div id="app-title" className="navbar-brand">FunTrip</div>
-        <ul className="nav navbar-nav">
-          <li className="active"><a href="#">Overview</a></li>
-          <li><a href="#">City 1</a></li>
-          <li><a href="#">City 2</a></li>
-          <li><a href="#">City 3</a></li>
-        </ul>
-      </nav>
-      <div className='col-md-12'>
-        <Login loggedIn={this.state.loggedIn} handleLogin={this.handleLogin}/>
-        <Logout loggedIn={this.state.loggedIn} user={this.state.user} handleLogout={this.handleLogout} />
+      <div className='.container-fluid'>
+        <nav className='navbar navbar-default bg-faded'>
+          <div id="app-title" className="navbar-brand">FunTrip</div>
+          <ul className="nav navbar-nav">
+            <li className="active"><a href="#">Overview</a></li>
+            <li><a href="#">City 1</a></li>
+            <li><a href="#">City 2</a></li>
+            <li><a href="#">City 3</a></li>
+          </ul>
+        </nav>
+        <div className='col-md-12'>
+          <Login loggedIn={this.state.loggedIn} handleLogin={this.handleLogin}/>
+          <Logout loggedIn={this.state.loggedIn} user={this.state.user} handleLogout={this.handleLogout} />
+        </div>
+        <button type="button" className="btn btn-primary" onClick = {() => { this.setState({sideBarOn: !this.state.sideBarOn}); }}>
+          <i class="glyphicon glyphicon-align-left"></i>
+        Toggle Sidebar
+        </button>
+        {this.state.sideBarOn ? <SideBar trips = {this.state.trips} userName = {'user1'} /> : null}
+        <div className="main col-md-12">
+          <CurrentInfo/>
+          <ReservationList items = {this.state.lastTrip.reservations}/>
+          <PrepList preparationItems = {this.state.lastTrip.preparationItems}/>
+          <ObjList handleObjAdd={this.handleObjAdd}/>
+          <MapView />
+        </div>
       </div>
-      <button type="button" className="btn btn-primary" onClick = {() => { this.setState({sideBarOn: !this.state.sideBarOn}); }}>
-      <i class="glyphicon glyphicon-align-left"></i>
-      Toggle Sidebar
-      </button>
-      {this.state.sideBarOn ? <SideBar trips = {this.state.trips} userName = {'user1'} /> : null}
-      <div className="main col-md-12">
-        <CurrentInfo/>
-        <ReservationList items = {this.state.lastTrip.reservations}/>
-        <PrepList />
-        <ObjList handleObjAdd={this.handleObjAdd}/>
-        <MapView />
-      </div>
-    </div>
     );
   }
 }
