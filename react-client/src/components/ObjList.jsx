5490f83185e1235632815943b9c8d071871d52a6
Kenny Cao <kennyxcao@gmail.com>
2017-10-04 03:23:36 -0700

    Implement and refactor objectives list, item and modal

--- react-client/src/components/ObjList.jsx
+++ react-client/src/components/ObjList.jsx
@@ -1,97 +1,23 @@
 import React from 'react';
 import ObjItem from './ObjItem.jsx';
-class ObjList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [
-        { 
-          id: 1,
-          description: 'Visit local restaurants',
-          status: 'unchecked'
-        },
-        { 
-          id: 2,
-          description: 'Eiffel Tower',
-          status: 'unchecked'
-        },
-        { 
-          id: 3,
-          description: 'The Louvre',
-          status: 'unchecked'
-        }
-      ],
-      add: false,
-      name: '',
-      category: ''
-    };
-    this.handleOptions = this.handleOptions.bind(this);
-  }
-  handlePlusClick() {
-    var add = this.state.add;
-    this.setState({
-      add: !add
-    });
-  }
-  handleInput(e) {
-    var name = e.target.value;
-    this.setState({
-      name: name
-    });
-  }
-  handleOptions(e) {
-    var category = e.target.value; 
-    this.setState({
-      category: category
-    });
-  }
-  handleAddBtn() {
-    var name = this.state.name;
-    var category = this.state.category;
-    var newObjItem = {
-      name: name,
-      category: category
-    };
-    this.props.handleObjAdd(newObjItem);
-    this.setState({
-      add: false
-    });
-  }
-  render() {
-    return (
-      <div> 
-        <h3 className="componentTitle">Objectives <button onClick={this.handlePlusClick.bind(this)} type="button" className="btn btn-default btn-xs">+</button></h3> 
-        <div> 
-          There are {this.state.list.length} things I want to do
-        </div>
-        <div> 
-         {this.state.list.map((item, index) =>
-            <ObjItem item={item} key={index}/>
-          )}
-        </div>
-        { 
-          (this.state.add) ? 
-          (<div>I want to: <input type="text" onChange={this.handleInput.bind(this)}/>
-            <select 
-             defaultValue={this.state.category} 
-             onChange={this.handleOptions}>
-              <option>Choose a category</option>
-              <option value="Sightseeing">Sightseeing</option>
-              <option value="Food to try">Food to try</option>
-              <option value="Activities">Activities</option>
-            </select>    
-          <button type="button" className="btn btn-default btn-xs" onClick={this.handleAddBtn.bind(this)}>add!</button> </div>) : ''
-        }
-      </div>
-    );
-  }
-}
-// {
-// <select value={this.state.category} onChange={this.handleOptions.bind(this)}> 
-            
-//             <option value="Sightseeing">Sightseeing</option>
-//             <option value="Food to try">Food to try</option>
-//             <option value="Activities">Activities</option>
-//           </select>
-// }
+import ObjModal from './ObjModal.jsx';
+
+const ObjList = (props) => (
+  <div className='objective-list'>
+    <ObjModal handleObjAdd={props.handleObjAdd} destinations={props.destinations}/>
+    <div> 
+      There are {props.objectives.length} things I want to do
+    </div>
+    <div>
+      {props.objectives.map((objective, index) => 
+        <ObjItem 
+          objective={objective} 
+          key={index}
+          handleObjItemChange={props.handleObjItemChange}
+          handleObjItemDelete={props.handleObjItemDelete}
+        />)}    
+    </div>  
+  </div>
+);
+
 export default ObjList;
