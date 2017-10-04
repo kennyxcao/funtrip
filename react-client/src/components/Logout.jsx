ad3be54f84a7641189703c9b15753df1adb002a9
Kenny Cao <kennyxcao@gmail.com>
2017-10-03 23:40:14 -0700

    Refactor Login and Logout JSX using react-boostrap

--- react-client/src/components/Logout.jsx
+++ react-client/src/components/Logout.jsx
@@ -1,11 +1,11 @@
+import {Button, Glyphicon} from 'react-bootstrap';
 import React from 'react';
 
 const Logout = (props) => (
   <div>
     {!props.loggedIn ? null : 
       <div className='logout'>
-        <h4>Welcome Back {props.user.toUpperCase()}!</h4>
-        <button type="button" className="btn btn-primary" onClick={props.handleLogout}>Logout</button> 
+        <h4>Welcome Back {props.user.toUpperCase()}! <Button bsStyle='primary' bsSize='small' onClick={props.handleLogout}><Glyphicon glyph='log-out' /></Button></h4>
       </div>
     }
   </div>
