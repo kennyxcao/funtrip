cf1060c1d565169bc1559cfdaac4dd2c4fa42139
Kenny Cao <kennyxcao@gmail.com>
2017-10-05 14:52:34 -0700

    Implement destination add and delete handler and modals

--- react-client/src/components/DestinationItem.jsx
+++ react-client/src/components/DestinationItem.jsx
@@ -1,8 +1,34 @@
-import {NavItem} from 'react-bootstrap';
-const React = require('react');
+import {Button, Glyphicon, NavItem} from 'react-bootstrap';
+import React from 'react';
 
-const DestinationItem = (props) => (
-  <NavItem eventKey={3 + props.key} href="#">{props.destination.name}</NavItem>
-);
+class DestinationItem extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      hover: false
+    };
+    this.mouseEnter = this.mouseEnter.bind(this);
+    this.mouseLeave = this.mouseLeave.bind(this);
+  }
+
+  mouseEnter () {
+    this.setState({hover: true});
+  }
+
+  mouseLeave () {
+    this.setState({hover: false});
+  }
+
+  render() {
+    return (
+      <NavItem className='destination-item' eventKey={3 + this.props.key} href="#" onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave}>
+        {this.props.destination.name}
+        {this.state.hover ? 
+          <Button bsStyle="default" bsSize="xsmall" onClick={() => this.props.handleDestinationDelete(this.props.destination._id)}><Glyphicon glyph="remove"/></Button>
+        : null}
+      </NavItem>
+    );
+  }
+}
 
 export default DestinationItem;
