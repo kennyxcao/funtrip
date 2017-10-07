9fa7b7885ca735f53aeb590a6e265d08d5de592d
Kenny Cao <kennyxcao@gmail.com>
2017-10-07 15:41:16 -0700

    Fix objective category select to rerender based on both current destination and objective category

--- react-client/src/components/ObjList.jsx
+++ react-client/src/components/ObjList.jsx
@@ -6,12 +6,12 @@ const ObjList = (props) => (
   <div className='objective-list'>
     <ObjModal handleObjAdd={props.handleObjAdd} destinations={props.destinations}/>
     <Navbar>
-    <Nav>
-      <NavItem eventKey={1} href="#" value='all' onClick={props.handleCategorySelect.bind(null, 'all')}>All</NavItem>
-      <NavItem eventKey={1} href="#" value='sightseeing' onClick={props.handleCategorySelect.bind(null, 'sightseeing')}>Sightseeing</NavItem>
-      <NavItem eventKey={2} href="#" value='food' onClick={props.handleCategorySelect.bind(null, 'food')}>Food</NavItem>
-      <NavItem eventKey={2} href="#" value='activities' onClick={props.handleCategorySelect.bind(null, 'activities')}>Activities</NavItem>
-    </Nav>
+      <Nav>
+        <NavItem eventKey={1} href="#" value='all' onClick={props.handleObjCategorySelect.bind(null, 'all')}>All</NavItem>
+        <NavItem eventKey={2} href="#" value='sightseeing' onClick={props.handleObjCategorySelect.bind(null, 'sightseeing')}>Sightseeing</NavItem>
+        <NavItem eventKey={3} href="#" value='food' onClick={props.handleObjCategorySelect.bind(null, 'food')}>Food</NavItem>
+        <NavItem eventKey={4} href="#" value='activities' onClick={props.handleObjCategorySelect.bind(null, 'activities')}>Activities</NavItem>
+      </Nav>
     </Navbar>
     <div> 
       There are {props.objectives.length} things I want to do
