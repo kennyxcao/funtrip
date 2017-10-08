62679367d3f451beaf35eabf9b52866ab7721792
Kenny Cao <kennyxcao@gmail.com>
2017-10-08 02:06:55 -0700

    Implement trip dates based on destinations and total trip dates in overview mode

--- react-client/src/components/CurrentInfo.jsx
+++ react-client/src/components/CurrentInfo.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 class CurrentInfo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+    };
   }
+
   render() {
     return (
       <div className='current-info'>
-        <h3 className = 'componentTitle'> Current Information </h3>
+        <h3> Current Information </h3>
         <hr className='divider'/>
         <p> Current TripId: {this.props.trip._id} </p>
+        <p> Trip Dates: <Moment format='YYYY/MM/DD' date={this.props.startDate}/> - <Moment format='YYYY/MM/DD' date={this.props.endDate}/></p>
         <p> Weather Info : its hot </p>
         <p> Currency Info : it's expensive </p>
       </div>
