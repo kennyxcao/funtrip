f8b08989f7a6f57f4fd62db5e85d157431456ce8
Kenny Cao <kennyxcao@gmail.com>
2017-10-07 23:31:59 -0700

    Change objective category select to use tabs instead of navbar

--- react-client/src/components/ObjList.jsx
+++ react-client/src/components/ObjList.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import ObjItem from './ObjItem.jsx';
 import ObjModal from './ObjModal.jsx';
-import {Grid, Row, Col, PageHeader, Navbar, Nav, NavDropdown, NavItem, MenuItem} from 'react-bootstrap';
+import {Tabs, Tab} from 'react-bootstrap';
+
 const ObjList = (props) => (
   <div className='objective-list'>
     <ObjModal handleObjAdd={props.handleObjAdd} destinations={props.destinations}/>
     <hr className='divider'/>    
-    <Navbar>
-      <Nav>
-        <NavItem eventKey={1} href="#" value='all' onClick={props.handleObjCategorySelect.bind(null, 'all')}>All</NavItem>
-        <NavItem eventKey={2} href="#" value='sightseeing' onClick={props.handleObjCategorySelect.bind(null, 'sightseeing')}>Sightseeing</NavItem>
-        <NavItem eventKey={3} href="#" value='food' onClick={props.handleObjCategorySelect.bind(null, 'food')}>Food</NavItem>
-        <NavItem eventKey={4} href="#" value='activities' onClick={props.handleObjCategorySelect.bind(null, 'activities')}>Activities</NavItem>
-      </Nav>
-    </Navbar>
+    <Tabs defaultActiveKey={0} id="obj-category-tabs" onSelect={props.handleObjCategorySelect}>
+      <Tab eventKey={0} title='all'></Tab>
+      <Tab eventKey={1} title='sightseeing'></Tab>
+      <Tab eventKey={2} title='food'></Tab>
+      <Tab eventKey={3} title='activities'></Tab>
+    </Tabs>
     <div> 
       There are {props.objectives.length} things I want to do
     </div>
