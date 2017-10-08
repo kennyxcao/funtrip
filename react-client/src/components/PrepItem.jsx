48e5ab10648d31c687c7930658d7a778e0985a93
Kenny Cao <kennyxcao@gmail.com>
2017-10-08 16:56:03 -0700

    Render due date for prepartion items and change the dates to italic and smaller size font

--- react-client/src/components/PrepItem.jsx
+++ react-client/src/components/PrepItem.jsx
@@ -1,4 +1,5 @@
 import {Checkbox, Button, Glyphicon} from 'react-bootstrap';
+import Moment from 'react-moment';
 import React from 'react';
 
 class PrepItem extends React.Component {
@@ -23,7 +24,7 @@ class PrepItem extends React.Component {
     return (
       <div className='prep-item' onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave}>
         <Checkbox checked={this.props.preparation.checked} onChange={() => this.props.handlePrepItemChange(this.props.preparation._id, !this.props.preparation.checked)}>
-          {this.props.preparation.name}
+          {this.props.preparation.name} <span className='italic'>due on <Moment format='MM/DD' date={this.props.preparation.dueDate}/></span>
           {this.state.hover ? 
             <Button bsStyle="default" bsSize="xsmall" onClick={() => this.props.handlePrepItemDelete(this.props.preparation._id)}><Glyphicon glyph="remove"/></Button>
             : null}
